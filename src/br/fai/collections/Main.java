088e247afde237fdc35b3a5f21900148f692ca2a
Ana Terezinha <aterezinha.19@hotmail.com>
2020-03-09 20:30:28 -0300

    primeiro commit

--- src/br/fai/collections/Main.java
+++ src/br/fai/collections/Main.java
@@ -1,10 +1,37 @@
 package br.fai.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Main app = new Main();
+		app.start();
 
 	}
+	
+	private Scanner scanner = new Scanner(System.in);
 
+	private void start() {
+		List<String> lista = new ArrayList<String>();
+		
+		String valorRecebido = obterDados();
+		System.out.println("O valor recebido foi: " + 
+		valorRecebido);
+	}
+	
+	
+	private String obterDados() {
+		
+		System.out.println("Digite o valor: ");
+		
+		int valor = scanner.nextInt();
+		return String.valueOf(valor);
+		
+		
+	}
+	
 }
